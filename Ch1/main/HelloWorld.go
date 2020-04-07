936d4755ac8c12be74adb33d54c9de22190b73f3
PatriciaHudakova <phudakova7@gmail.com>
2020-04-07 13:27:45 +0100

    Chapter 1 Complete

---
+++ Ch1/main/HelloWorld.go
@@ -0,0 +1,7 @@
+package main  //defines an executable rather than a library
+
+import "fmt"
+
+func main() {  //automatically executed just like the main method in Java
+	fmt.Println("Hello World ^.^")
+}
